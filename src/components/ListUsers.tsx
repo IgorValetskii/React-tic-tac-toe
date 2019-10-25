29d3d5c455054bc4c9e7f49fdff0611fe072e14b
IgorValetskii <blrapok@gmail.com>
2019-10-25 18:13:57 +0300

    add tic-tac-toe game

---
+++ src/components/ListUsers.tsx
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class List extends Component {
+    render() {
+        return (
+            <p>List of Users</p>
+        );
+    }
+}
+
+export default List;
